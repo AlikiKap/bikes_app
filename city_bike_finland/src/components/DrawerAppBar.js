2e8815985f0822130b91f017c3766e439f714ceb
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-07-22 21:35:17 +0300

    navigation bar and journey cards using material ui were implemented

---
+++ city_bike_finland/src/components/DrawerAppBar.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const navItems = ['Journeys', 'Stations'];
+
+function DrawerAppBar() {
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            City Bike Finland
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button component={Link} to={`/${item}`} key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+
+      </Box>
+    </Box>
+  );
+}
+
+
+
+export default DrawerAppBar;
--- city_bike_finland/src/components/Journey.js
+++ city_bike_finland/src/components/Journey.js
@@ -1,17 +1,44 @@
+import { CardContent, Typography } from '@mui/material'
+import Card from '@mui/material/Card'
+
 export default function Journey({journey}){
     return(
-        <div key={journey.journey_id} style={{
-          border: "solid",
-          margin: "1rem"
-        }}>
-        <p>Departure: {journey.departure}</p>
-        <p>Return: {journey.return}</p>
-        <p>Departure Station ID: {journey.departure_station_id}</p>
-        <p>Departure Station Name: {journey.departure_station_name}</p>
-        <p>Return Station ID: {journey.return_station_id}</p>
-        <p>Return Station Name: {journey.return_station_name}</p>
-        <p>Covered Distance: {journey.covered_distance}</p>
-        <p>Duration: {journey.duration}</p>
-      </div>
+        <Card key={journey.journey_id} sx={{minWidth: 275}}>
+          <CardContent>
+            <Typography variant="body1">Departure: {string2date(journey.departure)}</Typography>
+            <Typography variant="body1">Return: {string2date(journey.return)}</Typography>
+            <Typography variant="body1">Departure Station ID: {journey.departure_station_id}</Typography>
+            <Typography variant="body1">Departure Station Name: {journey.departure_station_name}</Typography>
+            <Typography variant="body1">Return Station ID: {journey.return_station_id}</Typography>
+            <Typography variant="body1">Return Station Name: {journey.return_station_name}</Typography>
+            <Typography variant="body1">Covered Distance: {journey.covered_distance / 1000} km</Typography>
+            <Typography variant="body1">Duration: {seconds2time(journey.duration)} min</Typography>
+          </CardContent>
+      </Card>
     )
+}
+
+function seconds2time(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+    
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+function string2date(inputDateString){
+
+const dateObject = new Date(inputDateString);
+
+const day = dateObject.getUTCDate().toString().padStart(2, "0");
+const month = (dateObject.getUTCMonth() + 1).toString().padStart(2, "0"); 
+const year = dateObject.getUTCFullYear();
+
+const hours = dateObject.getUTCHours().toString().padStart(2, "0");
+const minutes = dateObject.getUTCMinutes().toString().padStart(2, "0");
+
+const formattedDate = `${day}/${month}/${year}`;
+const formattedTime = `${hours}:${minutes}`;
+
+return(`${formattedTime}, ${formattedDate}`);
 }
