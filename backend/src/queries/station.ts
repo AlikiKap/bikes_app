35e8f9b73ad51866c54106071d3c656eb2b44ed6
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-08-05 00:19:44 +0300

    sql request for station list was edited

--- backend/src/queries/station.ts
+++ backend/src/queries/station.ts
@@ -2,7 +2,7 @@ import {pool} from '../util/db';
 import { Station } from './types';
 
 export const getAllStations=(req: any, res: any) => {
-    getStations()
+  getStationsNamesWithId()
     .then(response => {
       res.status(200).send(response);
     })
@@ -11,12 +11,32 @@ export const getAllStations=(req: any, res: any) => {
     })
   }
 
-  const getStations = (): Promise<string[]> => new Promise<string[]>(function (resolve, reject) {
-    pool.query('SELECT station_name AS departure_or_return_station FROM (SELECT departure_station_name AS station_name FROM journeys_05 UNION SELECT return_station_name AS station_name FROM journeys_05 ) AS unique_stations;', (error: Error, results: any) => {
-      if (error) 
-        return reject(error);
-  
-      const stations: string[] = results.rows.map((row: any) => row.departure_or_return_station);
-      resolve(stations);
-    });
+  const getStationsNamesWithId = (): Promise<Station[]> => new Promise<Station[]>(function (resolve, reject) {
+    pool.query(`SELECT station_id, station_name AS station_name FROM (
+      SELECT departure_station_id AS station_id, departure_station_name AS station_name FROM journeys_05 
+      UNION 
+      SELECT return_station_id AS station_id, return_station_name AS station_name FROM journeys_05 
+  ) AS unique_stations;`, (error: Error, results: any) => {
+    if (error) 
+      return reject(error)
+    
+      resolve(results.rows as Station[]);
+    
   });
+  });
+
+//   const getStation = (req: any, res: any) => {
+
+//     pool.query('SELECT  FROM journeys_05 where _id = $1 LIMIT 1', ).then(value => {
+
+//         if (!value.rows.length) return res.status(204).send()
+//         res.json(value.rows)
+
+//     }).catch(reason => {
+//         res.status(400).json({
+//             name: reason.name,
+//             message: reason.message
+//         })
+//     })
+
+// }
