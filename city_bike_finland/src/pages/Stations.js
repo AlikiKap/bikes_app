29e846b05a5fdce3a23fce8d768e3175917f39f3
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-08-05 00:25:41 +0300

    Cleanup

--- city_bike_finland/src/pages/Stations.js
+++ city_bike_finland/src/pages/Stations.js
@@ -1,15 +1,7 @@
 import { Stack } from '@mui/material';
-import { Grid } from '@mui/material';
-import Station from "../components/Station";
-
 import StationsItem from '../components/StationsItem';
 
-  
-
 export default function Stations() {
-    
-    
-
     return (
             <Stack
                 justifyContent="center"
