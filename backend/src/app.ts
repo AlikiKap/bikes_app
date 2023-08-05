0c5c07ffd765d138ca1dffb6b1df02f2c33c7056
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-08-05 22:04:41 +0300

    added route to single station view

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,19 +2,17 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-import { getAllJourneys } from '../src/queries/journey';
-import {getAllStations} from '../src/queries/station'
-
+import { getAllJourneys } from '../src/routes/journey';
+import { getAllStations, getStationData } from '../src/routes/station';
 import * as middlewares from './middlewares';
 import api from './api';
 import { Routes } from './queries/types';
-
 
 const app = express();
 
 app.use(cors({
   origin: '*',
-  optionsSuccessStatus: 200 
+  optionsSuccessStatus: 200
 }));
 
 app.use(morgan('dev'));
@@ -28,11 +26,12 @@ app.get<{}, Routes>('/', (req, res) => {
     stations: 'http://localhost:3001/stations'
   });
 });
-app.get('/journeys',getAllJourneys);
-app.get('/stations',getAllStations)
+
+app.get('/journeys', getAllJourneys);
+app.get('/stations', getAllStations);
+app.get('/stations/:id', getStationData);
 
 app.use('/api/v1', api);
-
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
