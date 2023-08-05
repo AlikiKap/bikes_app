d8b1f4b5bdc43afd0553b3cb17846bf21f74f162
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-08-05 22:02:55 +0300

    added single station view interface

--- backend/src/queries/types.ts
+++ backend/src/queries/types.ts
@@ -4,17 +4,25 @@ export interface Routes {
 }
 
 export interface Journey {
-    journey_departure: string;
-    journey_return: string;
-    departure_station_id: number;
-    departure_station_name: string;
-    return_station_id: number;
-    return_station_name: string;
-    covered_distance: number;
-    duration: number;
-  }
+  journey_departure: string;
+  journey_return: string;
+  departure_station_id: number;
+  departure_station_name: string;
+  return_station_id: number;
+  return_station_name: string;
+  covered_distance: number;
+  duration: number;
+}
 
-  export interface Station {
-    station_name: string;
-    station_id: number;
-  }
+export interface Station {
+  station_name: string;
+  station_id: number;
+}
+
+export interface SingleStation {
+  station_name: string;
+  station_id: number;
+  return_count: number;
+  departure_count: number;
+
+}
