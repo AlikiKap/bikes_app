398d2acf622fb324a808df04399c4fe2549c411b
AlikiKap <aliki02.kapasakalidi@gmail.com>
2023-08-06 21:14:58 +0300

    Implemented pagination on backend level

--- backend/src/routes/journey.ts
+++ backend/src/routes/journey.ts
@@ -1,7 +1,12 @@
 import { getJourneysData } from "../queries/journey";
 
+const itemsPerPage = 20;
+
 export const getAllJourneys = (req: any, res: any) => {
-    getJourneysData().then(response => {
+    const { page } = req.query;
+    const offset = ((parseInt(page) - 1) * itemsPerPage)
+
+    getJourneysData(itemsPerPage, offset).then(response => {
         res.status(200).send(response);
     }).catch(error => {
         res.status(500).send(error);
